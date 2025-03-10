39b9a6bf63c3a7f1f193a8beaa9b098ff9022fa9
ayushch80 <ayushch80@gmail.com>
2025-03-10 10:52:15 +0530

    refactor(scraping): optimized comment scraping

--- extension/src/js/content.js
+++ extension/src/js/content.js
@@ -1,19 +1,20 @@
 const videoId = window.location.href.split('/').pop().split('?')[1].split('=')[1];
 const comments = [];
-alert(videoId);
+// alert(videoId);
 
 function scrapeComments() {
     let commentList = [];
     let commentElements = document.querySelectorAll("#comment");
 
-    commentElements.forEach((commentElement) => {
+    for (let i = comments.length; i < commentElements.length; i++) {
+        let commentElement = commentElements[i];
         let body = commentElement.querySelector("#body");
         let main = body.querySelector("#main");
 
         let header = main.querySelector("#header");
         let author = header.querySelector("#header-author");
         let authorName = author.querySelector("#author-text").innerText.trim();
-        
+
         let expander = main.querySelector("#expander");
         let commentText = expander.querySelector("#content-text").innerText
 
@@ -23,10 +24,14 @@ function scrapeComments() {
             element: commentElement
         };
         commentList.push(comment);
-    });
+    }
     return commentList;
 }
 
 setInterval(() => {
-    console.log(scrapeComments());
+    if (comments.length < document.querySelectorAll("#comment").length) {
+        let scrapedComments = scrapeComments();
+        comments.push(...scrapedComments);
+        console.log(comments);
+    }
 }, 1000);
