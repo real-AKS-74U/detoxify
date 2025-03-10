5ddea6456803f96bd61bf4e4fa596b27b7037123
keysha30 <swastika.bhattacharyya30@gmail.com>
2025-03-10 15:20:35 +0530

    wip report added initial database and parsing

--- backend/main.py
+++ backend/main.py
@@ -3,9 +3,17 @@ import sqlite3
 app=Flask(__name__)
 
 def init_db():
-    conn = sqlite3.connect('comments.db')
+    conn = sqlite3.connect('db/reports.db')
     cursor = conn.cursor()
-    cursor.execute
+    cursor.execute('''  CREATE TABLE IF NOT EXISTS reports (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            text TEXT NOT NULL,
+            user TEXT,
+            videoId TEXT      
+                   )''')
+    conn.commit()
+    conn.close()
+    
 
 @app.route('/filter', methods=['POST'])
 def filter():
@@ -29,7 +37,55 @@ def filter():
         return jsonify({"success": False})
 
 
+@app.route('/report', methods=['POST'])
+def report():
+    try:
+        data = request.get_json()
+        
+        conn = sqlite3.connect('db/reports.db')
+        cursor = conn.cursor()
+
+        for commentData in data:
+            cursor.execute('''
+                        INSERT INTO reports (text, user, videoId ) 
+                        VALUES (?, ?, ?)
+                    ''', (commentData['text'], commentData['author'], commentData['videoId']))
+        conn.commit()
+        conn.close()
+        return jsonify({"success": True, "message": "Comments saved successfully."})
+        
+
+    except:
+        pass
+
+@app.route('/view_reports', methods=['GET'])
+def view_reports():
+    try:
+        conn = sqlite3.connect('db/reports.db')
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM reports")
+        rows = cursor.fetchall()
+
+        conn.close()
+
+        report_list = []
+        for row in rows:
+            report_list.append({
+                "id": row[0],
+                "text": row[1],
+                "user": row[2],
+                "videoId": row[3]
+            })
+
+        return jsonify({"success": True, "data": report_list})
+    except Exception as e:
+        print(e)
+        return jsonify({"success": False, "error": str(e)})
+
 
 if __name__ == '__main__':
+    init_db()
     app.run(debug=True)
 
+    
